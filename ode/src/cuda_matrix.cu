15f77bc49739504ea0ce3215807413478051816d
Mark <markandrusroberts@gmail.com>
2011-11-16 22:31:56 -0600

    Cleaned

--- ode/src/cuda_matrix.cu
+++ ode/src/cuda_matrix.cu
@@ -8,15 +8,6 @@
 #include "util.h"
 #include "config.h"
 
-#define CHECK_BANK_CONFLICTS 0
-#if CHECK_BANK_CONFLICTS
-#define AS(i, j) cutilBankChecker(((dReal*)&As[0][0]), (BLOCK_SIZE * i + j))
-#define BS(i, j) cutilBankChecker(((dReal*)&Bs[0][0]), (BLOCK_SIZE * i + j))
-#else
-#define AS(i, j) As[i][j]
-#define BS(i, j) Bs[i][j]
-#endif
-
 void cuda_checkError(const char *msg)
 {
 	cudaError_t err = cudaGetLastError();
@@ -186,81 +177,9 @@ template <int BLOCK_SIZE> __global__ void MatMulKernel(cuda_Matrix A, cuda_Matri
 	SetElement(C_sub, row, col, C_val);
 }
 
-template <int BLOCK_SIZE> __global__ void
-matrixMul( dReal* C, dReal* A, dReal* B, int wA, int wB)
-{
-    // Block index
-    int bx = blockIdx.x;
-    int by = blockIdx.y;
-
-    // Thread index
-    int tx = threadIdx.x;
-    int ty = threadIdx.y;
-
-    // Index of the first sub-matrix of A processed by the block
-    int aBegin = wA * BLOCK_SIZE * by;
-
-    // Index of the last sub-matrix of A processed by the block
-    int aEnd   = aBegin + wA - 1;
-
-    // Step size used to iterate through the sub-matrices of A
-    int aStep  = BLOCK_SIZE;
-
-    // Index of the first sub-matrix of B processed by the block
-    int bBegin = BLOCK_SIZE * bx;
-
-    // Step size used to iterate through the sub-matrices of B
-    int bStep  = BLOCK_SIZE * wB;
-
-    // Csub is used to store the element of the block sub-matrix
-    // that is computed by the thread
-    dReal Csub = 0;
-
-    // Loop over all the sub-matrices of A and B
-    // required to compute the block sub-matrix
-    for (int a = aBegin, b = bBegin;
-             a <= aEnd;
-             a += aStep, b += bStep) {
-
-        // Declaration of the shared memory array As used to
-        // store the sub-matrix of A
-        __shared__ dReal As[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Declaration of the shared memory array Bs used to
-        // store the sub-matrix of B
-        __shared__ dReal Bs[BLOCK_SIZE][BLOCK_SIZE];
-
-        // Load the matrices from device memory
-        // to shared memory; each thread loads
-        // one element of each matrix
-        AS(ty, tx) = A[a + wA * ty + tx];
-        BS(ty, tx) = B[b + wB * ty + tx];
-
-        // Synchronize to make sure the matrices are loaded
-        __syncthreads();
-
-        // Multiply the two matrices together;
-        // each thread computes one element
-        // of the block sub-matrix
-#pragma unroll
-        for (int k = 0; k < BLOCK_SIZE; ++k)
-            Csub += AS(ty, k) * BS(k, tx);
-
-        // Synchronize to make sure that the preceding
-        // computation is done before loading two new
-        // sub-matrices of A and B in the next iteration
-        __syncthreads();
-    }
-
-    // Write the block sub-matrix to device memory;
-    // each thread writes one element
-    int c = wB * BLOCK_SIZE * by + BLOCK_SIZE * bx;
-    C[c + wB * ty + tx] = Csub;
-}
-
 void cuda_dMultiply0(dReal *dev_A, dReal *dev_B, dReal *dev_C, int p, int q, int r)
 {
-	const int block_size = 4;
+	const int block_size = 2;
 
 	cuda_Matrix A;
 	A.width = r;
@@ -283,18 +202,6 @@ void cuda_dMultiply0(dReal *dev_A, dReal *dev_B, dReal *dev_C, int p, int q, int
 	dim3 dimBlock(block_size, block_size);
 	dim3 dimGrid(B.width / dimBlock.x, A.height / dimBlock.y);
 	MatMulKernel<2><<<dimGrid, dimBlock>>>(B, C, A);
-	/*
-	unsigned int uiWC, uiHC;
-	uiWC = 2 * block_size;
-	uiHC = 4 * block_size;
-	dim3 threads(block_size, block_size);
-	dim3 grid(uiWC / threads.x, uiHC / threads.y);
-	if (block_size == 4) {
-		matrixMul<4><<< grid, threads >>>(dev_B, dev_C, dev_A, r, r);
-	} else {
-		matrixMul<4><<< grid, threads >>>(dev_B, dev_C, dev_A, r, r);
-	}
-	*/
 }
 
 void cuda_dMultiply1(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r)
