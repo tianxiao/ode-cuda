b294631a35181b42d26abd5f930753d23e062171
Mark <markandrusroberts@gmail.com>
2011-11-16 19:08:31 -0600

    Adding functions

--- include/ode/cuda_matrix.h
+++ include/ode/cuda_matrix.h
@@ -8,7 +8,15 @@ extern "C" {
 #endif
 
 ODE_API void cuda_testMemcpy();
-ODE_API void cuda_dSetZero(dReal *a, int n);
+ODE_API void cuda_dSetZero(dReal *dev_a, int n);
+ODE_API void cuda_dSetValue(dReal *dev_a, int n, dReal value);
+ODE_API void cuda_dMultiply0(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r);
+ODE_API void cuda_dMultiply1(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r);
+ODE_API void cuda_dMultiply2(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r);
+
+ODE_API dReal *cuda_copyToDevice(dReal *a, int n);
+ODE_API dReal *cuda_copyFromDevice(dReal *dev_a, dReal *a, int n);
+ODE_API void cuda_freeFromDevice(dReal *dev_a);
 
 #ifdef __cplusplus
 }
--- ode/src/cuda_matrix.cu
+++ ode/src/cuda_matrix.cu
@@ -8,7 +8,7 @@
 #include "util.h"
 #include "config.h"
 
-void checkCUDAError(const char *msg)
+void cuda_checkError(const char *msg)
 {
 	cudaError_t err = cudaGetLastError();
 	if( cudaSuccess != err) {
@@ -48,6 +48,28 @@ void cuda_testMemcpy()
 	cudaFree(a_d); cudaFree(b_d);
 }
 
+dReal *cuda_copyToDevice(dReal *a, int n)
+{
+	dReal *dev_a;
+	cudaMalloc((void**) &dev_a, sizeof(dReal)*n);
+	cuda_checkError("malloc");
+	cudaMemcpy(dev_a, a, sizeof(dReal)*n, cudaMemcpyHostToDevice);
+	cuda_checkError("memcpy");
+	return dev_a;
+}
+
+dReal *cuda_copyFromDevice(dReal *dev_a, dReal *a, int n)
+{
+	cudaMemcpy(a, dev_a, sizeof(float)*n, cudaMemcpyDeviceToHost);
+	cuda_checkError("memcpy");
+	return a;
+}
+
+void cuda_freeFromDevice(dReal *dev_a)
+{
+	cudaFree(dev_a);
+}
+
 __global__ void setzero(dReal *a, int n)
 {
 	int tid = blockIdx.x;
@@ -55,25 +77,53 @@ __global__ void setzero(dReal *a, int n)
 		a[tid] = 0;
 }
 
-void cuda_dSetZero(dReal *a, int n)
+__global__ void setvalue(dReal *a, int n, dReal value)
+{
+	int tid = blockIdx.x;
+	if(tid < n)
+		a[tid] = value;
+}
+
+void cuda_dSetZero2(dReal *a, int n)
 {
 	dReal *dev_a; 
 
 	// allocate memory on GPU
 	cudaMalloc((void**) &dev_a, n*sizeof(dReal));
-	checkCUDAError("malloc");
+	cuda_checkError("malloc");
 
 	//copy array from CPU to GPU (not necessary)
 	cudaMemcpy(dev_a, a, n*sizeof(dReal), cudaMemcpyHostToDevice);
-	checkCUDAError("memcpy");
+	cuda_checkError("memcpy");
 
 	//fill array with 0 on the gpu
 	setzero<<<n,1>>>(dev_a, n);
 
 	//copy array of 0's 'a' from GPU to CPU
 	cudaMemcpy(a, dev_a, n*sizeof(dReal), cudaMemcpyDeviceToHost);
-	checkCUDAError("memcpy");
+	cuda_checkError("memcpy");
 
 	cudaFree(dev_a);		
 }
 
+void cuda_dSetZero(dReal *dev_a, int n)
+{
+	setzero<<<n,1>>>(dev_a, n);
+}
+
+void cuda_dSetValue(dReal *dev_a, int n, dReal value)
+{
+	setvalue<<<n,1>>>(dev_a, n, value);
+}
+
+void cuda_dMultiply0(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r)
+{
+}
+
+void cuda_dMultiply1(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r)
+{
+}
+
+void cuda_dMultiply2(dReal *dev_A, const dReal *dev_B, const dReal *dev_c, int p, int q, int r)
+{
+}
